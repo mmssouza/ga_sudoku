06f43109ee4c78adb473785bb53433092562aedd
Marcelo M. S. Souza <marcelo.mssouza@gmail.com>
2021-10-13 10:00:46 -0300

    Train and test sudoku dataset. Improved random number generation

--- geneticalgorithm/ga_sudoku.py
+++ geneticalgorithm/ga_sudoku.py
@@ -14,14 +14,17 @@ class ga_sudoku(ga):
                                        'crossover_type':'uniform',\
                                        'max_iteration_without_improv':None},\
                      convergence_curve=False,\
-                         progress_bar=True):
+                         progress_bar=True,\
+                         sg=np.random.SeedSequence()):
 
+  self.rgen = np.random.Generator(np.random.PCG64(sg))
 
   self.board = board
+
   super().__init__(function,dimension = 81,variable_type = 'int',variable_boundaries = np.array([[1,9]]*81,dtype='int'),variable_type_mixed = None,function_timeout = 10,algorithm_parameters=algorithm_parameters,convergence_curve=convergence_curve, progress_bar=progress_bar)
 
  def perm(self,x,msk):
-  x[~msk] = np.random.permutation(x[~msk])
+  x[~msk] = self.rgen.permutation(x[~msk])
   return x
 
  def init(self):
@@ -46,7 +49,7 @@ class ga_sudoku(ga):
            j1,j2 = 3*j,3*j+3
            tmp = aux[i1:i2,j1:j2]
            msk = self.Mask[i1:i2,j1:j2]
-           tmp[~msk] = np.random.permutation(list({1,2,3,4,5,6,7,8,9} - set(tmp[msk]))) 
+           tmp[~msk] = self.rgen.permutation(list({1,2,3,4,5,6,7,8,9} - set(tmp[msk]))) 
 
          self.var = aux.flatten().copy()
          self.solo[0:self.dim] = self.var.copy()
@@ -65,12 +68,12 @@ class ga_sudoku(ga):
     for j in [0,1,2]:
      i1,i2 = i,i+3
      j1,j2 = 3*j,3*j+3
-     ran=np.random.random()
+     ran=self.rgen.random()
      if ran < 0.5:
       ofs1[i1:i2,j1:j2] = Y[i1:i2,j1:j2].copy()
       ofs2[i1:i2,j1:j2] = X[i1:i2,j1:j2].copy() 
   elif c_type == 'one_point':
-    p = np.random.randint(0,8)
+    p = self.rgen.integers(0,8)
     k = 0
     for i in [0,3,6]:
      for j in [0,1,2]:
@@ -82,8 +85,8 @@ class ga_sudoku(ga):
       k = k + 1 
   elif c_type == 'two_point':
     k = 0
-    p1 = np.random.randint(0,9)
-    p2 = np.random.randint(p1,9)
+    p1 = self.rgen.integers(0,9)
+    p2 = self.rgen.integers(p1,9)
     for i in [0,3,6]:
      for j in [0,1,2]:
       i1,i2 = i,i+3
@@ -106,13 +109,13 @@ class ga_sudoku(ga):
     aux = ofs[i1:i2,j1:j2].copy()
     msk = self.Mask[i1:i2,j1:j2]
     if m_type == 'perm':
-     if np.random.random() < self.prob_mut:
+     if self.rgen.random() < self.prob_mut:
       ofs[i1:i2,j1:j2] = self.perm(aux,msk) 
     elif m_type == 'swap':
      aux = aux.flatten()
      for k in range(9):
-      if np.random.random() < self.prob_mut:
-       l = np.random.randint(9)
+      if self.rgen.random() < self.prob_mut:
+       l = self.rgen.integers(9)
        if (k != l) and ~(msk.flatten()[k] or msk.flatten()[l]):
         tmp = aux[k]
         aux[k] = aux[l]
--- geneticalgorithm/sudoku_solver.py
+++ geneticalgorithm/sudoku_solver.py
@@ -3,7 +3,7 @@
 import numpy as np
 from ga_sudoku import ga_sudoku as ga
 from multiprocessing import Pool
-
+import pickle
 
 #board = np.array(   [0, 0, 4, 5, 1, 0, 7, 0, 8, 
 #                     0, 0, 0, 0, 0, 0, 9, 0, 0, 
@@ -15,15 +15,22 @@ from multiprocessing import Pool
 #                     0, 0, 6, 0, 0, 0, 0, 0, 0, 
 #                     4, 0, 7, 0, 3, 5, 8, 0, 0],dtype='int')
                     
-board = np.array(  [0, 4, 2, 0, 0, 0, 0, 0, 5, 
-                    0, 0, 0, 6, 3, 2, 0, 8, 0, 
-                    0, 8, 0, 0, 4, 0, 2, 0, 0, 
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 
-                    7, 1, 5, 0, 6, 8, 3, 4, 0, 
-                    9, 0, 8, 3, 5, 0, 7, 6, 1, 
-                    0, 9, 1, 0, 0, 6, 0, 0, 0, 
-                    0, 0, 0, 0, 2, 0, 1, 9, 0, 
-                    0, 0, 6, 1, 0, 0, 0, 5, 0],dtype='int')                      
+#board = np.array(  [0, 4, 2, 0, 0, 0, 0, 0, 5, 
+#                    0, 0, 0, 6, 3, 2, 0, 8, 0, 
+#                    0, 8, 0, 0, 4, 0, 2, 0, 0, 
+#                    0, 0, 0, 0, 0, 0, 0, 0, 0, 
+#                    7, 1, 5, 0, 6, 8, 3, 4, 0, 
+#                    9, 0, 8, 3, 5, 0, 7, 6, 1, 
+#                    0, 9, 1, 0, 0, 6, 0, 0, 0, 
+#                    0, 0, 0, 0, 2, 0, 1, 9, 0, 
+#                    0, 0, 6, 1, 0, 0, 0, 5, 0],dtype='int')                      
+
+board_name ='image1000.dat'
+dataset_fname ='v2_train.pkl'
+
+d = pickle.load(open(dataset_fname,'rb'))
+
+board = d[board_name].flatten()
 
 def f(X):
 
@@ -39,24 +46,26 @@ def f(X):
  return sum_row.sum() + sum_col.sum() 
 
 def task(p):
- np.random.seed()
- model=ga(function=f,board=board,algorithm_parameters = p,progress_bar = False)
+ model=ga(function=f,board=board,algorithm_parameters = p[0],progress_bar = False,sg = p[1])
  model.run()
- return (p,model.best_variable,model.report)
+ return (p[0],model.best_variable,model.report)
 
-N = 25
-params = {'max_num_iteration': 2700,\
-                   'population_size': 500,\
-                   'mutation_probability': 0.2,\
+N = 30
+
+ss = np.random.SeedSequence(12345)
+
+params = {'max_num_iteration': 5700,\
+                   'population_size': 1350,\
+                   'mutation_probability': 0.26,\
                    'elit_ratio': 0.3,\
-                   'crossover_probability': 0.9,\
+                   'crossover_probability': 0.8,\
                    'parents_portion': 0.4,\
                    'crossover_type':'uniform',\
-                   'max_iteration_without_improv':None}
+                   'max_iteration_without_improv':2500}
 
 if __name__ == '__main__':
- with Pool(processes=3) as pool:
-  res = pool.map(task,[params for i in range(N)])
+ with Pool(processes=4) as pool:
+  res = pool.map(task,[(params,s) for s in ss.spawn(N)])
   for i in res:
    print("\n********************************")
    print('\n',i[0])
